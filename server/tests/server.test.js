b6b78d41faab2deac9c376c93459b4d9d4abe3dc
Lionel <lionelrenier@sfr.fr>
2017-09-08 17:37:01 +0200

    add test DELETE /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,3 +104,43 @@ describe('GET /todos/:id', () => {
     .end(done);
   });
 });
+
+describe('DELETE /todos/:id', () => {
+  it('should remove a todo', (done) => {
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+    .delete(`/todos/${hexId}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexId);
+    })
+    .end((err, res) => {
+      if (err) {
+        return done(err);
+      }
+      Todo.findById(hexId).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => done(e));
+
+    });
+  });
+
+  it('should return a 404 if todo not found', (done) => {
+    request(app)
+    .delete(`/todos/${id.toHexString()}`)
+    .expect(404)
+    // .expect((res) => {
+    //   expect(res.body.id).toNotMatch(todos[0]._id);
+    // })
+    .end(done);
+  });
+
+  it('should return 404 if object is invalid', (done) => {
+    request(app)
+    .delete(`/todos/123mlk`)
+    .expect(404)
+    .end(done);
+  });
+});

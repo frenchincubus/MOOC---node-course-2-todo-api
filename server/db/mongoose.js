7e3aff17475ad80c60828040641f7129b51f6704
Lionel <lionelrenier@sfr.fr>
2017-09-08 17:19:04 +0200

    Add DELETE /todos/:id route

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -4,7 +4,7 @@ mongoose.Promise = global.Promise;
 
 const url = 'mongodb://root:root@ds145283.mlab.com:45283/todos';
 
-mongoose.connect( 'mongodb://localhost:27017/TodoApp', { useMongoClient : true });
+mongoose.connect( url || 'mongodb://localhost:27017/TodoApp', { useMongoClient : true });
 // si une adresse est créée à l'intérieur de l'hébergeur : process.env.MONGODB_URI
 
 module.exports = {

5f5e1d500009dcac09613327877b51d74b59b3be
Lionel <lionelrenier@sfr.fr>
2017-09-08 15:39:52 +0200

    Set up app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', { useMongoClient : true });
+mongoose.connect('mongodb://<root>:<root>@ds145283.mlab.com:45283/todos' || 'mongodb://localhost:27017/TodoApp', { useMongoClient : true });
+// si une adresse est créée à l'intérieur de l'hébergeur : process.env.MONGODB_URI
 
 module.exports = {
   mongoose: mongoose

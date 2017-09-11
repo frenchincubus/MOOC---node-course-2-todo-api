bbde2c96c1153c750a61a141038cbcabfe754298
Lionel <lionelrenier@sfr.fr>
2017-09-11 15:28:57 +0200

    Add POST /users route and token metho

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-process.env.MONGODB_URI = 'mongodb://root:root@ds145283.mlab.com:45283/todos';
+
 
 mongoose.connect( process.env.MONGODB_URI , { useMongoClient : true });
 // si une adresse est créée à l'intérieur de l'hébergeur : process.env.MONGODB_URI

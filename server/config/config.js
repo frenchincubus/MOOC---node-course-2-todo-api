b596ea968436ec0a1eacc70ef18af90801347ef7
Lionel <lionelrenier@sfr.fr>
2017-09-12 16:42:02 +0200

    update config to use json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,12 +1,14 @@
 var env = process.env.NODE_ENV || 'development';
 console.log('env *******', env);
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-} else {
-  process.env.MONGODB_URI = 'mongodb://root:root@ds145283.mlab.com:45283/todos';
+if(env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
+
+
+//  process.env.MONGODB_URI = 'mongodb://root:root@ds145283.mlab.com:45283/todos';
